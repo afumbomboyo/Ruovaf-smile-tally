04d74dc67619a6db18fb6a14342a6f622833582a
Afu Boyo <afumbomboyo@gmail.com>
2026-04-09 19:54:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9145861812-e1bb5",
+  "appId": "1:388303989953:web:d412cabcebef4821ab7840",
+  "apiKey": "AIzaSyCcxcDPYQ7qId8vIjVi-k7HLX3FjzXoPzs",
+  "authDomain": "studio-9145861812-e1bb5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "388303989953"
+};
